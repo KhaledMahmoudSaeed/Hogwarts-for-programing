d9eef9a389300a13d37c0b9b307f9796e00c80b8
KhaledMahmoudSaeed <khaild22k12m71f@gmail.com>
2025-03-24 08:43:46 +0200

    feat: use Functions helpers

--- public/index.php
+++ public/index.php
@@ -3,8 +3,10 @@ require "../vendor/autoload.php";
 
 use App\Core\Router;
 use Helpers\Path;
+use Helpers\Functions;
 
 $path = new Path();// create an instance for root path
+$fn = new Functions();
 $router = new Router();
 require_once("../app/Core/routes.php");// use all allowed routes
 
