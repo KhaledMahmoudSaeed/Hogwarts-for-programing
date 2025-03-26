5a3429befeda7a38b791d58dffdf0eb9be285fff
KhaledMahmoudSaeed <khaild22k12m71f@gmail.com>
2025-03-26 09:33:45 +0200

    feat: add user CRUD routes

--- app/Core/routes.php
+++ app/Core/routes.php
@@ -1,6 +1,6 @@
 <?php
 // create all our routers 
-
+$id = 1;
 $router->get('/', 'index.php');
 $router->get('/login', 'Authentication/login.view.php');
 $router->get('/register', 'Authentication/register.view.php');
@@ -10,7 +10,13 @@ $router->post('/logout', 'Authentication/logout.php');
 $router->get('/logout', 'Authentication/logout.php');// for now 
 
 $router->get('/dashboard', "Dashboard/index.php");
+
 $router->get('/dashboard/users', "Dashboard/users/index.php");
+$router->get('/dashboard/user', 'Dashboard/users/show.php');
+$router->get('/dashboard/user/edit', 'Dashboard/users/edit.php');
+$router->put('/dashboard/users/update', "Dashboard/users/update.php");
+$router->delete('/dashboard/users/delete', "Dashboard/users/destroy.php");
+
 $router->get('/dashboard/wands', "Dashboard/wands/index.php");
 $router->get('/dashboard/courses', "Dashboard/courses/index.php");
 $router->get('/dashboard/purchases', "Dashboard/purchases/index.php");
