02a00839e551b07bd1c6e38e8f55eb18d0542fc6
KhaledMahmoudSaeed <khaild22k12m71f@gmail.com>
2025-03-31 05:53:26 +0200

    feat: make all controllers for enrollment

---
+++ app/Http/Controllers/Student/enrolls/index.php
@@ -0,0 +1,21 @@
+<?php
+use Helpers\Auth;
+session_start();
+$id = Auth::getAuthenticatedUser()['id'];
+$courses = $db->getWith2Joins(
+    "courses",
+    "id",
+    "user_id",
+    "enrollments",
+    "course_id",
+    "users",
+    "id",
+    [
+        "enrollments.id" => "id",
+        "courses.name" => "name",
+        "users.name" => "professor",
+        "users.email" => "email",
+    ],
+    "enrollments.user_id =$id "
+);
+$path->view("student/enrolls/index.php", $courses);
