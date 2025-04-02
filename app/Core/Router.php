8651ec124eb6a737fef61e94db4157b235bcb54d
KhaledMahmoudSaeed <khaild22k12m71f@gmail.com>
2025-04-03 01:14:39 +0200

    feat: make a middleware method to apply middleware to all routes

--- app/Core/Router.php
+++ app/Core/Router.php
@@ -1,77 +1,84 @@
 <?php
 
 namespace App\Core;
+
+use App\Core\Middleware\Middleware;
 use Helpers\Path;
 class Router
 {
     private $routers = [];
 
     // method to make it easy add routes with no redendancy
-    public function add($uri, $controller, $method)
+    public function add($uri, $controller, $method, $middleware = 'auth')
     {
         $this->routers[] = [
             "uri" => $uri,
             "controller" => $controller,
-            "method" => $method
+            "method" => $method,
+            'middleware' => $middleware
         ];
         return $this;
     }
-    //handel all get routes
-    public function get($uri, $controller)
+    public function addMiddleware($middleware)
     {
-        return $this->add($uri, $controller, "GET");
+        $this->routers[array_key_last($this->routers)]['middleware'] = $middleware;
+    }
+    //handel all get routes
+    public function get($uri, $controller, $middleware = 'auth')
+    {
+        return $this->add($uri, $controller, "GET", $middleware);
     }
     //handel all post routes
 
-    public function post($uri, $controller)
+    public function post($uri, $controller, $middleware = 'auth')
     {
-        return $this->add($uri, $controller, "POST");
+        return $this->add($uri, $controller, "POST", $middleware);
     }
     //handel all put routes
-    public function put($uri, $controller)
+    public function put($uri, $controller, $middleware = 'auth')
     {
-        return $this->add($uri, $controller, "PUT");
+        return $this->add($uri, $controller, "PUT", $middleware);
     }
     //handel all delete routes
-    public function delete($uri, $controller)
+    public function delete($uri, $controller, $middleware = 'auth')
     {
-        return $this->add($uri, $controller, "DELETE");
+        return $this->add($uri, $controller, "DELETE", $middleware);
     }
 
-    public function resources($uri, $controller)
+    public function resources($uri, $controller, $middleware = 'auth')
     {
         //index   /dashboard/users  
-        $this->get($uri, $controller);
+        $this->get($uri, $controller, $middleware);
 
         //show    /dashboard/user?id=2
         $uri = substr($uri, 0, -1);
         $controller = substr_replace($controller, "show.php", -9);
-        $this->get($uri, $controller);
+        $this->get($uri, $controller, $middleware);
 
         //edit    /dashboard/user/edit
         $uri .= "/edit";
         $controller = substr_replace($controller, "edit.php", -8);
-        $this->get($uri, $controller);
+        $this->get($uri, $controller, $middleware);
 
         //update  /dashboard/user/update
         $uri = substr_replace($uri, "update", -4);
         $controller = substr_replace($controller, "update.php", -8);
-        $this->put($uri, $controller);
+        $this->put($uri, $controller, $middleware);
 
         //create   /dashboard/user/create
         $uri = substr_replace($uri, "create", -6);
         $controller = substr_replace($controller, "create.php", -10);
-        $this->get($uri, $controller);
+        $this->get($uri, $controller, $middleware);
 
         //store   /dashboard/user/store
         $uri = substr_replace($uri, "store", -6);
         $controller = substr_replace($controller, "store.php", -10);
-        $this->post($uri, $controller);
+        $this->post($uri, $controller, $middleware);
 
         //delete  /dashboard/user/delete 
         $uri = substr_replace($uri, "delete", -5);
         $controller = substr_replace($controller, "destroy.php", -9);
-        $this->delete($uri, $controller);
+        $this->delete($uri, $controller, $middleware);
     }
 
     // route the router to correspond controller
@@ -90,7 +97,24 @@ class Router
         $uriWithoutQuery = explode('?', $uri)[0];
         foreach ($this->routers as $router) {
             if ($router['uri'] === $uriWithoutQuery && $router['method'] === $method) {
-                return require($path->base_path("app/Http/Controllers/" . $router['controller']));
+                $message = (new Middleware)->resolve($router['middleware'], $uriWithoutQuery);
+                if ($message === "200") {
+                    $controllerPath = $path->base_path("app/Http/Controllers/" . $router['controller']);
+                    // to handel if route is exits and controller not exits when using resource method
+                    try {
+                        if (!file_exists($controllerPath)) {
+                            $this->abort();
+                        }
+
+                        // var_dump($controllerPath);
+                        // exit;
+                        return require $controllerPath;
+                    } catch (\Throwable $th) {
+                        $this->abort();
+                    }
+                } else {
+                    $this->abort($message);
+                }
             }
         }
         $this->abort(404);
