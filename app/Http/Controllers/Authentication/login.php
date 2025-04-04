2d09db52ee258825f1505bdb0cb64b44f85d492f
KhaledMahmoudSaeed <khaild22k12m71f@gmail.com>
2025-04-04 07:48:31 +0200

    feat:send img data to session

--- app/Http/Controllers/Authentication/login.php
+++ app/Http/Controllers/Authentication/login.php
@@ -16,16 +16,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if ($user[0] && password_verify($password, $user[0]['password'])) {
         $id = $user[0]['id'];
         $role = $user[0]['role'];
+        $img = $user[0]['img'];
         $house_id = $db->getOne("houses", $user[0]['house_id'])['name'];
         $payload = array(
             "id" => $id,
             "role" => $role,
             "house" => $house_id,
+
             "iat" => time(),
             "exp" => time() + (7 * 3600),
         );
         $jwt = JWT::encode($payload, $_ENV['JWT_SECRET_KEY'], "HS256");
         $_SESSION['jwt'] = $jwt;
+        $_SESSION['img'] = $img;
         header("Location: /home");
         exit;
     } else {
