2fb83a2e3409d22949cf84e5a4bba203482a2e66
KhaledMahmoudSaeed <khaild22k12m71f@gmail.com>
2025-05-01 21:57:24 +0200

    fix: solve delete img error and check whether professor update img or not and validate name of item

--- app/Http/Controllers/Dashboard/users/destroy.php
+++ app/Http/Controllers/Dashboard/users/destroy.php
@@ -1,7 +1,8 @@
 <?php
+$user = $db->getOne("users", $_POST['id']);
 $db->delete('users', $_POST['id']);
 $users = ["user1.png", "user2.png", "user3.png", "user4.png"];
-if (!in_array($_POST['img'], $users)) {
-    $fn->deleteImage("users", $_POST['img']);
+if (!in_array($user['img'], $users)) {
+    $fn->deleteImage("users", $user['img']);
 }
 header("Location: /dashboard/users");
